f2613851e97488e18f10ff8ae2dd3c9e91776da2
heinrich-oocl <heinrich.siu@oocl.com>
2022-12-16 17:16:07 +0800

    feat: init project

---
+++ src/main/java/com/movinder/be/SpringBootMovinderApplication.java
@@ -0,0 +1,12 @@
+package com.movinder.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMovinderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMovinderApplication.class, args);
+    }
+
+}
